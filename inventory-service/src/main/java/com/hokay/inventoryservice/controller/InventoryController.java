a51c1f07aae6814c29201a50acb4237d2667294f
Hakan Okay <hokay.ca@gmail.com>
2024-01-26 12:21:30 -0500

    Changed how GET inventory endpoints behaves
    It's now using Request Parameters instead of path variables. This way we can send multiple skuCodes to be processed by the InventoryService.

--- inventory-service/src/main/java/com/hokay/inventoryservice/controller/InventoryController.java
+++ inventory-service/src/main/java/com/hokay/inventoryservice/controller/InventoryController.java
@@ -1,10 +1,13 @@
 package com.hokay.inventoryservice.controller;
 
 
+import com.hokay.inventoryservice.dto.InventoryResponse;
 import com.hokay.inventoryservice.service.InventoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/inventory")
@@ -12,9 +15,9 @@ import org.springframework.web.bind.annotation.*;
 public class InventoryController {
     private final InventoryService inventoryService;
 
-    @GetMapping("/{sku-code}")
+    @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public boolean isInStock(@PathVariable("sku-code") String skuCode) {
+    public List<InventoryResponse> isInStock(@RequestParam List<String> skuCode) {
         return inventoryService.isInStock(skuCode);
     }
 }
--- inventory-service/src/main/java/com/hokay/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/hokay/inventoryservice/service/InventoryService.java
@@ -1,18 +1,38 @@
 package com.hokay.inventoryservice.service;
 
+import com.hokay.inventoryservice.dto.InventoryResponse;
+import com.hokay.inventoryservice.model.Inventory;
 import com.hokay.inventoryservice.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class InventoryService {
 
     private final InventoryRepository inventoryRepository;
 
     @Transactional(readOnly = true)
-    public boolean isInStock(String skuCode) {
-        return inventoryRepository.findBySkuCode(skuCode).isPresent();
+    public List<InventoryResponse> isInStock(List<String> skuCode) {
+        return inventoryRepository
+                .findBySkuCodeIn(skuCode)
+                .stream()
+                .map(this::mapToResponse)
+                .toList();
     }
+
+    private InventoryResponse mapToResponse(Inventory inventory) {
+        return InventoryResponse.builder()
+                .skuCode(inventory.getSkuCode())
+                .isInStock(inventory.getQuantity() > 0)
+                .build();
+    }
+
+
 }
