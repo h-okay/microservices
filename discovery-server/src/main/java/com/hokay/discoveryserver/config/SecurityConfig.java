3a2aeb156e0fa5ebd3fb8e9af68f034b3ebbad4b
Hakan Okay <hokay.ca@gmail.com>
2024-01-26 12:18:02 -0500

    Created discovery-server

---
+++ discovery-server/src/main/java/com/hokay/discoveryserver/config/SecurityConfig.java
@@ -0,0 +1,58 @@
+package com.hokay.discoveryserver.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Value("${eureka.username}")
+    String username;
+
+    @Value("${eureka.password}")
+    String password;
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User.builder()
+                .username(username)
+                .password(passwordEncoder().encode(password))
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authConfig -> authConfig
+                        .requestMatchers("/eureka/**")
+                        .permitAll()
+                        .anyRequest().authenticated())
+                .httpBasic(httpBasic -> {
+                })
+                .logout(logout -> {
+                    logout.logoutUrl("/logout");
+                    logout.addLogoutHandler((request, response, authentication) -> {
+                        authentication.setAuthenticated(false);
+                    });
+                });
+        return http.build();
+    }
+}
