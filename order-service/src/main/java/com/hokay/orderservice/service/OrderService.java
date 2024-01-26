3b057ad795f05f6954656cc36debbe927077c9a0
Hakan Okay <hokay.ca@gmail.com>
2024-01-26 12:19:34 -0500

    Add CircuitBreaker, Timeout and Retry annotations to order-service

--- order-service/src/main/java/com/hokay/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/hokay/orderservice/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.hokay.orderservice.service;
 
+import com.hokay.orderservice.dto.InventoryResponse;
 import com.hokay.orderservice.dto.OrderLineItemsDto;
 import com.hokay.orderservice.dto.OrderRequest;
 import com.hokay.orderservice.model.Order;
@@ -8,8 +9,11 @@ import com.hokay.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 
 @Service
@@ -18,8 +22,9 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final WebClient.Builder webClientBuilder;
 
-    public void placeOrder(OrderRequest orderRequest) {
+    public String placeOrder(OrderRequest orderRequest) throws IllegalArgumentException {
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
         List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemsDtoList()
@@ -27,7 +32,30 @@ public class OrderService {
                 .map(this::mapToDto)
                 .toList();
         order.setOrderLineItemsList(orderLineItems);
-        orderRepository.save(order);
+        List<String> skuCodes = order.getOrderLineItemsList()
+                .stream()
+                .map(OrderLineItems::getSkuCode)
+                .toList();
+
+        // Make a SYNCHRONOUS call to Inventory service and place order if product is in stock
+        InventoryResponse[] inventoryResponseArray = webClientBuilder.build()
+                .get()
+                .uri("http://inventory-service/api/inventory",
+                        uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
+                .retrieve()
+                .bodyToMono(InventoryResponse[].class)
+                .block(); // turn it into sync-request
+
+        boolean allProductsInStock = Arrays.stream(Objects.requireNonNull(inventoryResponseArray))
+                .allMatch(InventoryResponse::isInStock);
+
+        if (allProductsInStock) {
+            // Save the order to DB
+            orderRepository.save(order);
+            return "Order placed successfully!";
+        }
+
+        throw new IllegalArgumentException("Product is not in stock. Please try again later.");
     }
 
     private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
@@ -37,4 +65,5 @@ public class OrderService {
         orderLineItems.setSkuCode(orderLineItemsDto.getSkuCode());
         return orderLineItems;
     }
+
 }
